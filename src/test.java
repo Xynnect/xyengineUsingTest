67f722bcf576b98898f27efed641db98328b3e3f
Dian Yordanov <dido.jordanov7@gmail.com>
2015-03-14 14:49:47 +0000

    test

---
+++ src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by XelnectMobileUser on 3/14/2015.
+ */
+public class test {
+    public static void main(String[] args) {
+        xyengine.helloWorldExample();
+    }
+}
---
+++ src/xyengine.java
@@ -0,0 +1,20 @@
+/**
+ * Created by XelnectMobileUser on 3/14/2015.
+ */
+public abstract class xyengine {
+// For the swarm!!!
+//xyengine as it is now can be used for methods but it is not to be used for other types of information
+
+
+    public static void helloWorldExample(){
+        System.out.print("hello world");
+    }
+
+    public static void helloWorldExample1(){
+        System.out.print("hello world");
+    }
+
+    public static void helloWorldExample2(){
+        System.out.print("hello world");
+    }
+}
